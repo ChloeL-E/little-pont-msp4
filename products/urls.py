147c6b8b49dea83cfbd28f730271777e9244cea0
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-08-03 17:55:17 +0000

    Fixed bug in courses dropdown

--- products/urls.py
+++ products/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.babies_courses, name='babies_courses'),
-    path('', views.toddlers_courses, name='toddlers_courses'),
-    path('', views.preschooler_courses, name='preschooler_courses'),
-    path('', views.early_years_courses, name='early_years_courses'),
+    path('babies/', views.babies_courses, name='babies_courses'),
+    path('toddlers/', views.toddlers_courses, name='toddlers_courses'),
+    path('preschooler/', views.preschooler_courses, name='preschooler_courses'),
+    path('early_years/', views.early_years_courses, name='early_years_courses'),
 ]

fcbfcb890d44916aba1137d0e30575989c1a149a
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-08-12 20:42:48 +0000

    Added instructor details to admin site

--- instructor/admin.py
+++ instructor/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Instructor
+
+# Register models
+
+class InstructorAdmin(admin.ModelAdmin):
+    list_display = (
+        'instructor_name',
+        'bio_description',
+        'experience',
+        'speciality_age_group',
+    )
+
+    ordering = ('instructor_name',)
+
+admin.site.register(Instructor, InstructorAdmin)
 
 # Register your models here.

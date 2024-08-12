d701ff724c09611975b197270f123bcc0b003556
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-08-12 21:35:59 +0000

    Altered instructor view to reduce unnecessary code

--- instructor/urls.py
+++ instructor/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('instructor_one/', views.instructor_one, name='instructor_one'),
-    path('instructor_two/', views.instructor_two, name='instructor_two'),
-    path('instructor_three/', views.instructor_three, name='instructor_three'),
-    path('instructor_four/', views.instructor_four, name='instructor_four'),
+    path('instructor/<int:id>/', views.instructor_info, name='instructor_info')
 ]

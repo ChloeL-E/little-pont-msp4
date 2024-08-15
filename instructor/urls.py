74328a3abf195520d1c3702ad73c8ed5a95bb5e5
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-08-15 10:34:33 +0000

    Added checkout views, templates and fixed instructor image bug

--- instructor/urls.py
+++ instructor/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('instructor_info/', views.instructor_info, name='instructor_info'),
-    path('instructor_detail/<int:instructor_id>', views.instructor_detail, name='instructor_detail'),
+    path('', views.instructor_info, name='instructor_info'),
+    path('<instructor_id>', views.instructor_detail, name='instructor_detail'),
 ]

07d15fe39a4e35cf4c7ebe4f431fab70a9eed99f
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-08-27 11:17:04 +0000

    Added editing functionality to courses

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_courses, name='all_courses'),
     path('courses/<str:age_group>/', views.courses_by_age_group, name='courses_by_age_group'),
     path('add/', views.add_course, name='add_course'),
+    path('edit/<int:course_id>/', views.edit_course, name='edit_course'),
 ]
--- products/views.py
+++ products/views.py
@@ -79,3 +79,27 @@ def add_course(request):
     }
 
     return render(request, template, context)
+
+
+def edit_course(request, course_id):
+    """ Edit a course """
+    course= get_object_or_404(Course, pk=course_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=course)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated course!')
+            return redirect(reverse('all_courses'))
+        else:
+            messages.error(request, 'Failed to update course. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=course)
+        messages.info(request, f'You are editing {course.name}')
+
+    template = 'products/edit_course.html'
+    context = {
+        'form': form,
+        'course': course,
+    }
+
+    return render(request, template, context)

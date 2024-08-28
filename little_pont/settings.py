c7e92761747b32165fbe323643e3e6f9d539cf98
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-08-28 11:55:50 +0000

    Removed secret key and set debug

--- little_pont/settings.py
+++ little_pont/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g0u^e6@e5_d4#%pjt54@bp(s6=f67om0o1*vbhac-r9fxjt(wy'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'little-pont-role-play-855c26627652.herokuapp.com']
 

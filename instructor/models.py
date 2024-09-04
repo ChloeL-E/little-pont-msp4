6e06c64f91cf741c34e337ef4603dcf4a44b2a17
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-09-04 21:55:30 +0000

    Changed Instructor model intructor name field

--- instructor/models.py
+++ instructor/models.py
@@ -6,7 +6,7 @@ class Instructor(models.Model):
     Instructor model to manage information about the different team members.
     Used as FK within the course model
     """
-    instructor_name = models.CharField(max_length=5, null=False, blank=False)
+    instructor_name = models.CharField(max_length=50, null=False, blank=False)
     bio_description = models.TextField(null=False, blank=False)
     experience = models.TextField(null=False, blank=False)
     speciality_age_group = models.CharField(max_length=50, null=False, blank=False)

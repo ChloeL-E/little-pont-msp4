4156f6155e92bef273f7a54efa15e27b0f79f242
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-09-04 20:10:26 +0000

    Added admin.py to content

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import BookingEnquiry
 
-# Register your models here.
+# Register models
+
+class BookingEnquiryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'booking_username',
+        'booking_full_name',
+        'booking_email',
+        'subject',
+        'content',
+        'date_submitted',
+    )
+
+    ordering = ('-date_submitted',)
+
+admin.site.register(BookingEnquiry, BookingEnquiryAdmin)

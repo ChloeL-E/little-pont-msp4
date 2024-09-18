12dc9d60753710cce5aeab687cfc271032a46d5e
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-09-18 06:20:37 +0000

    Added responsive css for bag page

--- little_pont/settings.py
+++ little_pont/settings.py
@@ -14,8 +14,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-
-
 ALLOWED_HOSTS = [
     'localhost',
     '8000-chloele-littlepontmsp4-jfcyz069qfa.ws.codeinstitute-ide.net',

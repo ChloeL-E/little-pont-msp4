23bfc307e9c9231bbfe493f0a8424d6851125b7f
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-09-22 14:25:47 +0000

    Added form labels to booking enquiry form

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import BookingEnquiry
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field
 
 
 class BookingEnquiryForm(forms.ModelForm):
@@ -9,13 +11,16 @@ class BookingEnquiryForm(forms.ModelForm):
             "booking_username",
             "date_submitted",
         )
-
+        
     def __init__(self, *args, **kwargs):
         """
         Add placeholders, classes, and style to form fields.
+        sr-only classes added to form fields.
         Remove auto-generated labels and set autofocus on first field
         """
-        super().__init__(*args, **kwargs)
+        super(BookingEnquiryForm, self).__init__(*args, **kwargs)
+
+        # Define placeholders for the fields
         placeholders = {
             "booking_full_name": "Full Name",
             "booking_email": "Email",
@@ -23,23 +28,26 @@ class BookingEnquiryForm(forms.ModelForm):
             "content": "Tell us about your booking request",
         }
 
-        # Set autofocus on booking_full_name
+        # Initialize Crispy FormHelper for layout customization
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            # Apply 'sr-only' class to labels for screen readers
+            Field('booking_full_name', css_class="sr-only"),
+            Field('booking_email', css_class="sr-only"),
+            Field('subject', css_class="sr-only"),
+            Field('content', css_class="sr-only"),
+        )
+        # Set autofocus on name
         self.fields["booking_full_name"].widget.attrs["autofocus"] = True
-
-        # Customize all fields
         for field_name, field in self.fields.items():
             # Apply CSS classes and styles
             field.widget.attrs["class"] = "rounded-2"
             field.widget.attrs["style"] = (
                 "border: 1px solid #ffe6f2; box-shadow: 0 1px 3px 0 #ffe6f2;"
             )
-
             # Set placeholder text
             if field.required:
                 placeholder = f"{placeholders[field_name]} *"
             else:
                 placeholder = placeholders[field_name]
             field.widget.attrs["placeholder"] = placeholder
-
-            # Hide the label
-            field.label = False

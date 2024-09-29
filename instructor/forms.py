30f57926fb880a3aed79f0232fd09f6243092da8
Chloe Livingstone-Evans <chloe_c123@hotmail.co.uk>
2024-09-29 12:56:17 +0000

    Fixing server error on instructor app

--- instructor/forms.py
+++ instructor/forms.py
@@ -29,9 +29,11 @@ class InstructorForm(forms.ModelForm):
             "speciality_age_group": "Speciality Age Group",
             "image_url": "Image URL",
         }
-        # Set the id of the image field to 'new-image'
-        # self.fields['image'].widget.attrs.update({'id': 'new-image'})
-        
+
+        self.fields['image'].label = "Image"
+        # set image label to sr only
+        self.fields['image'].widget.attrs['class'] = "sr-only"
+
         self.fields["instructor_name"].widget.attrs[
             "autofocus"
         ] = True  # autofocus on instructor name field
@@ -42,9 +44,10 @@ class InstructorForm(forms.ModelForm):
             )
         # set the placeholders and remove any input labels
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f"{placeholders[field]} *"
-                self.fields[field].label = False
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs["placeholder"] = placeholder
+            if field != 'image':
+                if self.fields[field].required:
+                    placeholder = f"{placeholders[field]} *"
+                    self.fields[field].label = False
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs["placeholder"] = placeholder
